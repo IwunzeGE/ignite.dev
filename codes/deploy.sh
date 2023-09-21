239a92c87a4f511effae15a558c7e1ff30abde1d
IwunzeGE <gpdmillionaire@gmail.com>
2023-09-21 15:58:10 -0400

    added manifest.yml

--- codes/deploy.sh
+++ codes/deploy.sh
@@ -6,8 +6,6 @@ CLUSTER_NAME=ignite-dev
 # Create a KinD cluster
 kind create cluster --name=$CLUSTER_NAME
 
-# Set the KUBECONFIG environment variable to use the newly created cluster
-#export KUBECONFIG="$(kind get kubeconfig-path --name=$CLUSTER_NAME)"
 
 # Verify the cluster is running
 
